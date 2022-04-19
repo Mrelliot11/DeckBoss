7b09c96ece03067ecb9a54fa7544522e2b256ce3
ElliotDubrow <emn2345@gmail.com>
2022-04-19 10:34:44 -0500

    merging test and main repos

---
+++ routes/card-info.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var pokemon = require('pokemontcgsdk');
+
+var cardName = '';
+var imgUrl = '';
+var cardPrice = 0;
+
+pokemon.card.find('base1-4')
+.then(card => {
+  console.log(card); // testing to see returned json object
+  cardName = card.name;
+  imgUrl = card.images.small;
+  cardPrice = card.cardmarket.prices.averageSellPrice;
+  });
+
+router.get('/', function(req, res, next) {
+  res.render('card-info', { title: 'DeckBoss', name: 'Elliot' , card: cardName, cardImg: imgUrl, price: cardPrice});
+});
+
+module.exports = router;
