a99faa5e110ff94b58b75aa09fee892f9167fa71
Matt Wudi <wudi.matt@gmail.com>
2022-04-20 22:53:01 -0500

    Changed cardPrices into an object to allow access to multiple pricing options

--- routes/card-info.js
+++ routes/card-info.js
@@ -4,18 +4,21 @@ var pokemon = require('pokemontcgsdk');
 
 var cardName = '';
 var imgUrl = '';
-var cardPrice = 0;
+var cardPrices = {};
+var chosenPrice = 0;
 
 pokemon.card.find('base1-4')
 .then(card => {
   console.log(card); // testing to see returned json object
   cardName = card.name;
   imgUrl = card.images.small;
-  cardPrice = card.cardmarket.prices.averageSellPrice;
+  cardPrices = card.cardmarket.prices;
+  console.log(cardPrices);
   });
 
 router.get('/', function(req, res, next) {
-  res.render('card-info', { title: 'DeckBoss', name: 'Elliot' , card: cardName, cardImg: imgUrl, price: cardPrice});
+  res.render('card-info', 
+  { title: 'DeckBoss', name: 'Elliot' , card: cardName, cardImg: imgUrl, prices: cardPrices});
 });
 
 module.exports = router;
