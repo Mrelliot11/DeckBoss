42c2a99b5c5b810bddd886dc261dca1110fd13a3
ElliotDubrow <emn2345@gmail.com>
2022-04-21 13:31:08 -0500

    created login html and added dependencies for pswd

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var cardInfoRouter = require('./routes/card-info');
+
 
 var app = express();
 
@@ -18,10 +19,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/card-info', cardInfoRouter);
 
 // catch 404 and forward to error handler
