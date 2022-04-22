421dc84cccdfc2e08ce15819d95e14c28bbb339c
ElliotDubrow <emn2345@gmail.com>
2022-04-21 21:21:15 -0500

    removed unneeded dependancies

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 require('dotenv').config();
 
-const { Client } = require('pg');
-
-const connectionString = process.env.DATABASE_URL;
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: "DeckBoss", name: 'Elliot'});
