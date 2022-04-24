26115e2a6d622856378c76e2efb2e3726daa0b84
ElliotDubrow <emn2345@gmail.com>
2022-04-24 13:53:53 -0500

    added login and route to index page

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 require('dotenv').config();
 const { Client } = require('pg');
 //Needed so we can connect to the heroku database
@@ -15,10 +16,38 @@ const client = new Client({
 })
 client.connect(); //Connect to the database
 
+async function getUser(username) {
+  const query = `SELECT * FROM users WHERE username = '${username}'`;
+
+  return await client.query(query);
+}
+
 //Render the login page
 router.get('/', function(req, res, next) {
   res.render('login', {error: ''});
 });
 
+router.post('/', function(req, res, next) {
+
+  var username = req.body.username;
+  var password = req.body.password;
+
+  getUser(username).then(function(result) {
+    if (result.rows.length > 0) {
+      var user = result.rows[0];
+      var hash = user.hash;
+      var salt = user.salt;
+      var iterations = user.iterations;
+      var hash2 = crypto.pbkdf2Sync(password, salt, iterations, 64, 'sha512').toString('hex');
+      if (hash === hash2) {
+        res.redirect('/?username=' + username);
+      } else {
+        res.render('login', {error: 'Incorrect username or password'});
+      }
+    } else {
+      res.render('login', {error: 'Incorrect username or password'});
+    }
+  });
+});
 
 module.exports = router;
